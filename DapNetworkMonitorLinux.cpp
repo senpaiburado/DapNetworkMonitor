c79ccd83a51116ee7c2e283e06525cb45e0d7715
armatusmiles <akurotych@gmail.com>
2019-01-15 01:03:08 +0700

    [+] cbMonitorNotification, make DapNetworkMonitorLinux is singleton

--- DapNetworkMonitorLinux.cpp
+++ DapNetworkMonitorLinux.cpp
@@ -1,9 +1,37 @@
 #include "DapNetworkMonitorLinux.h"
 
-DapNetworkMonitorLinux::DapNetworkMonitorLinux(QObject *parent):
-    DapNetworkMonitorAbstract(parent), m_cmdProgram("ip", {"monitor"}, this)
+void DapNetworkMonitorLinux::cbMonitorNotification(const dap_network_notification_t notification)
 {
+    if(notification.type == IP_ADDR_ADD || notification.type == IP_ADDR_REMOVE) {
 
+        qDebug() << QString("Interface %1 %2 has IP address %3")
+                    .arg(notification.addr.interface_name)
+                    .arg((notification.type == IP_ADDR_ADD ? "now" : "no longer"))
+                    .arg(notification.addr.s_ip);
+
+
+    } else if(notification.type == IP_ROUTE_ADD || notification.type == IP_ROUTE_REMOVE) {
+        if (notification.type == IP_ROUTE_REMOVE) {
+            qDebug() << QString("Delete route to destination --> %1/%2 proto %3 and gateway %4")
+                        .arg(notification.route.s_destination_address)
+                        .arg(notification.route.netmask)
+                        .arg(notification.route.protocol)
+                        .arg(notification.route.s_gateway_address);
+
+        } else  if (notification.type == IP_ROUTE_ADD) {
+            qDebug() << QString("Adding route to destination --> %1/%2 proto %3 and gateway %4")
+                        .arg(notification.route.s_destination_address)
+                        .arg(notification.route.netmask)
+                        .arg(notification.route.protocol)
+                        .arg(notification.route.s_gateway_address);
+        }
+    }
+
+}
+
+DapNetworkMonitorLinux::DapNetworkMonitorLinux(QObject *parent):
+    DapNetworkMonitorAbstract(parent)
+{
 }
 
 bool DapNetworkMonitorLinux::isTunDriverInstalled() const
@@ -26,11 +54,20 @@ bool DapNetworkMonitorLinux::isOthersGatewayDefined() const
 void DapNetworkMonitorLinux::monitoringStart()
 {
     qDebug() << "Start network monitoring";
-    m_cmdProgram.start();
+    if(m_isMonitoringRunning == true) {
+        qWarning() << "Network monitoring already running";
+        return;
+    }
+
+    if(dap_network_monitor_init(cbMonitorNotification) == 0) {
+        m_isMonitoringRunning = true;
+    }
 }
 
 void DapNetworkMonitorLinux::monitoringStop()
 {
     qDebug() << "Stop network monitoring";
-    m_cmdProgram.stop();
+    dap_network_monitor_deinit();
+
+    m_isMonitoringRunning = false;
 }
--- DapNetworkMonitorLinux.h
+++ DapNetworkMonitorLinux.h
@@ -3,18 +3,26 @@
 
 #include "DapNetworkMonitorAbstract.h"
 #include "DapMonitorCmdProgram.h"
+#include "dap_network_monitor.h"
+
 
 class DapNetworkMonitorLinux : public DapNetworkMonitorAbstract
 {
     Q_OBJECT
 private:
-    DapMonitorCmdProgram m_cmdProgram;
+    static void cbMonitorNotification(const dap_network_notification_t notification);
+
+    DapNetworkMonitorLinux(QObject *parent = Q_NULLPTR);
+    DapNetworkMonitorLinux(const DapNetworkMonitorLinux&) = delete;
+    DapNetworkMonitorLinux& operator=(const DapNetworkMonitorLinux&) = delete;
 public:
-    explicit DapNetworkMonitorLinux(QObject *parent = Q_NULLPTR);
+    static DapNetworkMonitorLinux* instance()
+        {static DapNetworkMonitorLinux client; return &client;}
 
     bool isTunDriverInstalled() const override;
     bool isDapGatewayDefined() const override;
     bool isOthersGatewayDefined() const override;
+
 signals:
 
 public slots:
